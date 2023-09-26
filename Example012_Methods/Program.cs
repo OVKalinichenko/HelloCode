ce58d770ab88078932afb2e2aee09537e04dfcd8
Olga_Kalinichenko <okalinichenko@icloud.com>
2023-09-26 17:19:25 +0200

    Added new folders (Example012 and Homewok_sem3)

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,181 @@
+﻿// ====Виды методов
+
+// ====Вид 1
+
+// void Method1()
+// {
+//     System.Console.WriteLine("Автор программы ...");
+// }
+// Вызов метода
+// Method1();
+
+// ====Вид 2
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// // Вызов метода
+// Method2(msg: "Текст сообщения");
+
+// void Method2_1(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         System.Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Вызов метода
+// Method2_1(msg: "Messege", count: 4);
+// или
+// Method2_1(count: 5, msg: "New messege");
+
+// ==== Вид 3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// Вызов метода
+// int year = Method3();
+// System.Console.WriteLine(year);
+
+// ==== Вид 4
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// // Вызов метода
+// string res = Method4(10, "Ax");
+// System.Console.WriteLine(res);
+
+// ===== Примеры цикла for
+
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;  
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "Ax");
+// System.Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for ( int j = 2; j<= 10; j++)
+//     {
+//         System.Console.WriteLine($"{i} * {j } = {i*j}");
+//     } 
+//     System.Console.WriteLine();
+// }
+
+// ==== Работа с текстом
+// Дан текст. В тесте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заминить большими "К",
+// а большие "С" заменить маленькими "с".
+
+// string text = "- Я думал, - Сказал князь, улыбаясь, - что, "
+//             + "ежели бы вас поСлали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие пруССкого короля."
+//             + "Вы так краСноречивы. Вы дадите мне чай?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+// string Replace(string text, char oldValue, char newValuer)
+// {
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for ( int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValuer}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+
+// newText = Replace(newText, 'к', 'К');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+
+// newText = Replace(newText, 'С', 'с');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+
+// ==== Алгороитм сортировки методом выбора (min/max)
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         System.Console.Write($"{array[i]} ");
+//     } 
+//     System.Console.WriteLine();
+// }
+
+// ==== min
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if(array[j] < array[minPosition]) minPosition = j;
+//         }
+
+//         int temparory = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temparory;
+//     }
+// }
+
+
+// ==== max
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int maxPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if(array[j] > array[maxPosition]) maxPosition = j;
+//         }
+
+//         int temparory = array[i];
+//         array[i] = array[maxPosition];
+//         array[maxPosition] = temparory;
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
---
+++ Homework_Sem3/Program.cs
@@ -0,0 +1,103 @@
+﻿// Внутри класса Answer напишите метод IsPalindrome, который принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+
+// Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+
+// Для остальных чисел вернуть True или False.
+
+
+// 14212 -> False
+// 12821 -> True
+// 234322 -> Число не пятизначное
+//                      False
+
+
+// bool IsPalindrome(int number)
+//     {
+//       int un = number % 10;
+//       int tth = number / 10000;
+//       int th = number/ 1000 - tth * 10;
+//       int ten = ((number - un ) / 10) % 10;
+      
+//       if( number >= 10000 && number < 100000)
+//       {
+//         if(un == tth && th == ten) return true;
+//         else return false;
+//       }
+//       else
+//       {
+//         System.Console.WriteLine("Число не пятизначное");
+//         return false;
+//       }
+//       }
+
+// Console.Write("Enter five-digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(IsPalindrome(num));
+
+
+// Внутри класса Answer напишите метод DistanceBetweenPoints, который принимает на вход 
+// координаты двух точек pointA и pointB в виде массива целых чисел, и возвращает расстояние 
+// между ними в 3D пространстве.
+
+
+// A (3,6,8); B (2,1,-7) -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// double DistanceBetweenPoints(double[] pointA, double[] pointB)
+//     {
+//     return Math.Round(Math.Sqrt(Math.Pow(pointB[0] - pointA[0], 2) + Math.Pow(pointB[1] - pointA[1], 2) + Math.Pow(pointB[2] - pointA[2], 2)), 2, MidpointRounding.AwayFromZero);
+//     }
+
+
+// double DistanceBetweenPoints( double xa, double ya, double za, double xb, double yb, double zb)
+// {
+//     return Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+// }
+
+// System.Console.Write(" Enter coordinate x of A: ");
+// double xa = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write(" Enter coordinate y of A: ");
+// double ya = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write(" Enter coordinate z of A: ");
+// double za = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write(" Enter coordinate x of B: ");
+// double xb = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write(" Enter coordinate y of B: ");
+// double yb = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write(" Enter coordinate z of B: ");
+// double zb = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine($"Расстояние между точками А ({xa}, {ya}, {za}) и В ({xb}, {yb}, {zb}) -> {Math.Round(DistanceBetweenPoints(xa, ya, za, xb, yb, zb), 2, MidpointRounding.AwayFromZero)}");
+
+// Внутри класса Answer напишите метод ShowCube, который принимает на вход число (N) и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+
+
+// N = 3 
+// /*
+// 1
+// 8
+// 27
+// */
+
+// N = 5
+// /*
+// 1
+// 8
+// 27
+// 64
+// 125
+// */
+
+// void QuadTable(int N)
+// {
+//     int current = 1;
+//     while(current <= N)
+//         {
+//             System.Console.WriteLine(current * current * current);
+//             current++;
+//         }
+// }
+// System.Console.Write("Input N:  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write($" -> ");
+// QuadTable(n);
