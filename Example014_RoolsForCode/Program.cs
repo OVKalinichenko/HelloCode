86ef626bb7ebd357bd7b538a1f3aee93d71c3f20
Olga_Kalinichenko <okalinichenko@icloud.com>
2023-10-13 16:17:06 +0200

    Added folder Example014

---
+++ Example014_RoolsForCode/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+var a = 12;
+// System.Console.WriteLine(a.GetType().Name);
+var date = new int[] { 1, 2, 3, 4, }
+        .Where(e => e > 0)
+        .Select(e => new { q = e, w = e +1 });
+
+System.Console.WriteLine(date.GetType().Name);
